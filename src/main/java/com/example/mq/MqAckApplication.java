4c46cff263d7b4019c9db50ecd4cdbc9046ae773
zhong_zhangzhi <zhongzhangzhi@xiniaoyun.com>
2019-04-24 11:36:24 +0800

    Initial commit

---
+++ src/main/java/com/example/mq/MqAckApplication.java
@@ -0,0 +1,13 @@
+package com.example.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqAckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqAckApplication.class, args);
+    }
+
+}
